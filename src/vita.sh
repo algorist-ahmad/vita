ab7ae90bcd61d71c7f2b3285f7b01502ac0ad576
ahmad <b-9k@outlook.com>
2024-12-07 20:38:06 -0500

    first level parser fully operational, debug mode fully operational, script foundation complete. Next targets: secondary programs

--- src/vita.sh
+++ src/vita.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
 
-declare -A ARGS=(
-    [args]="$@"
-    [debug]=0
-)
-
-declare -A FILES=(
-    [data]="$SHEETS"
-    [config_file]="$HOME/.config/my_script/config.yml"
-    [log_dir]="$HOME/logs/"
-)
-
-declare -A FLAG=(
+declare -A ENV=(
+    [root]=$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")") # path to project root
+    [pwd]=$(pwd)
     [error]=0
+    [noarg]=0
+)
+
+declare -A FILE=(
+    [log]="$LOGS/vita.log"
+    [help]="${ENV[root]}/doc/help.txt"
 )
 
 declare -A ERROR=(
     [0]='OK'
-    [11]="CONTRADICTING ARGUMENTS: select only 1 of the following: ls, new, edit, del"
+    [150]='ERROR 1: desc'
+    [151]='ERROR 2: desc'
+    [152]='ERROR 3: desc'
+    [153]='ERROR 4: desc'
+    [154]='ERROR 5: desc'
+    [155]='ERROR 6: desc'
 )
+
+declare -A ARG=(
+        [input]="$@"
+        [debug]=0
+        [help]=0
+        [stat]=0
+        [config]='null'       # show, key:value
+        [job]='null'          # list, add, del, show, edit
+        [cv]='null'           # list, add, del, show, edit, clone, query, render
+        [template]='null'     # list, add, del, show, edit
+        [doc]='null'          # list, add, del, show
+        [render]='null'       # <resume-uuid> [--template <template-label>] [--output <file-path>]
+        [link]='null'         # <job-id> <resume-uuid>
+        [unknown]=''          # unknown args
+    )
 
 #########################################################
 
@@ -30,80 +47,89 @@ main() {
     terminate    # execute post-script tasks regardless of operation
 }
 
+# prepare program for execution
 initialize() {
-    # echo "Setting up environment"
-    # echo "Checking for missing variables or files"
-    FLAG[error]=0
+    # if no arg provided, get help
+    [[ -z "${ARG[input]}" ]] && ENV[noarg]=1 && ARG[help]=1
 }
 
 dissect() {
 
-    LAST_OPTION=''
+    local last_option='unknown'
     
     # Iterate over arguments using a while loop
     while [[ $# -gt 0 ]]; do
         case "$1" in
+
             debug | --debug)
-                NAMESPACE[debug]=1 ;
+                ARG[debug]=1 ;
                 ;;
-            list | ls | '')
-                NAMESPACE[select]=1 ;
-                ;;                
-            new | add | +*)
-                NAMESPACE[create]=1 ;
-                NAMESPACE[select]=0 ;
+            help | --help | -h)
+                ARG[help]=1 ;
                 ;;
-            edit | open | mod)
-                NAMESPACE[edit]=1 ;
-                NAMESPACE[select]=0 ;
+            stat)
+                ARG[stat]=1 ;
+                ;;               
+            config)
+                ARG[config]='' ;
+                last_option='config' ;
                 ;;
-            del* | rm | -*)
-                NAMESPACE[delete]=1 ;
-                NAMESPACE[select]=0 ;
+            job | -j)
+                ARG[job]='' ;
+                last_option='job' ;
                 ;;
-            dir)
-                NAMESPACE[dir]=1 ;
-                NAMESPACE[select]=0 ;
+            cv | -c)
+                ARG[cv]='' ;
+                last_option='cv' ;
+                ;;
+            template | -t)
+                ARG[template]='' ;
+                last_option='template' ;
+                ;;
+            doc | -d)
+                ARG[doc]='' ;
+                last_option='doc' ;
+                ;;
+            render | -r)
+                ARG[render]='' ;
+                last_option='render' ;
+                ;;
+            link | -l)
+                ARG[link]='' ;
+                last_option='link' ;
+                ;;
+            --)
+                last_option='unknown' ; # resets last option
                 ;;
             *)
-                uuid_or_filename "$1" ;
+                ARG[$last_option]="${ARG[$last_option]} $1" ;
                 ;;
-        esac
-        # discard argument
-        shift
+
+        esac ; shift # discard argument
     done
 }
 
 validate() {
-    check_for_contradiction
+    :
+    # check_for_error1
     # check_for_error2
-    # check_for_error3
     # ...
-}
-
-check_for_contradiction() {
-    local count=0
-    for key in select create edit delete; do
-        if [[ "${NAMESPACE[$key]}" -eq 1 ]]; then
-            ((count++))
-        fi
-    done
-
-    if [[ $count -gt 1 ]]; then FLAG[error]=11; fi
 }
 
 dispatch() {
 
-    if [[ "${FLAG[error]:-0}" -gt 0 ]]; then
-        echo "Error: ${ERROR[${FLAG[error]}]}" >&2
-        exit "${FLAG[error]}"
+    e="${ENV[error]}"
+    
+    if [[ $e -gt 0 ]]; then
+        echo "Error: $(get_error_msg $e)" >&2
+        exit $e
     fi
 
-    [[ "${NAMESPACE[select]}" -eq 1 ]] && do_select
-    [[ "${NAMESPACE[create]}" -eq 1 ]] && do_create
-    [[ "${NAMESPACE[edit]}"   -eq 1 ]] && do_edit
-    [[ "${NAMESPACE[delete]}" -eq 1 ]] && do_delete
-    [[ "${NAMESPACE[dir]}"    -eq 1 ]] && print_dir
+    [[ ${ARG[help]} -eq 1 ]] && print_help
+    # [[ "${ARG[create]}" -eq 1 ]] && do_create
+    # [[ "${ARG[edit]}"   -eq 1 ]] && do_edit
+    # [[ "${ARG[delete]}" -eq 1 ]] && do_delete
+    # [[ "${ARG[dir]}"    -eq 1 ]] && print_dir
     
     # ...else
     #     echo "Error: No valid operation specified." >&2
@@ -112,69 +138,85 @@ dispatch() {
 }
 
 terminate() {
-    reveal_variables
-    help_if_no_arg
+    code=$?
+    # if debug is true, reveal variables
+    [[ ${ARG[debug]} -eq 1 ]] && reveal_variables
+    exit $code
 }
 
-print_dir() {
-    echo "${FILES[data]}" | xclip -selection clipboard
-    echo "${FILES[data]}"
-    echo "(copied)"
+print_help() {
+    bat $(get_file help)
 }
 
-list_if_no_arg() {
-     [[ "${NAMESPACE[args]}" == "" ]] && NAMESPACE[select]=1
-}
-
-help_if_no_arg() {
-    [[ "${NAMESPACE[args]}" == "" ]] && echo 'Usage: sheet [new | mod | del] [UUID | FILENAME]' && exit 0
-}
-
+# Loop through the keys of the associative array and print key-value pairs
 reveal_variables() {
-    if [[ NAMESPACE['debug'] -eq 1 ]]; then
-        # Loop through the keys of the associative array and print key-value pairs
-        echo -e "\nDEBUG:"
-        for key in "${!NAMESPACE[@]}"; do
-            echo "Key: $key, Value: ${NAMESPACE[$key]}"
-        done
-    fi
+    local yellow="\033[33m"
+    local green="\033[32m"
+    local red="\033[31m"
+    local purple="\033[35m"
+    local reset="\033[0m"
+
+    echo -e "--- ARGUMENTS ---"
+    for key in "${!ARG[@]}"; do
+        value="${ARG[$key]}"
+        value="${value%"${value##*[![:space:]]}"}"  # Trim trailing whitespace
+        value="${value#"${value%%[![:space:]]*}"}"  # Trim leading whitespace
+        color="$reset"
+
+        if [[ $value == 'null' ]]; then
+            color=$purple    # Null value
+        elif [[ $value == '1' ]]; then
+            color=$green     # True value
+        elif [[ $value == '0' ]]; then
+            color=$red       # False value
+        fi
+
+        printf "${yellow}%-20s${reset} : ${color}%s${reset}\n" "$key" "$value"
+    done
+
+    echo -e "--- ENVIRONMENT ---"
+    for key in "${!ENV[@]}"; do
+        value="${ENV[$key]}"
+        value="${value%"${value##*[![:space:]]}"}"  # Trim trailing whitespace
+        value="${value#"${value%%[![:space:]]*}"}"  # Trim leading whitespace
+        color="$reset"
+
+        if [[ $value == 'null' ]]; then
+            color=$purple    # Null value
+        elif [[ $value == '1' ]]; then
+            color=$green     # True value
+        elif [[ $value == '0' ]]; then
+            color=$red       # False value
+        fi
+
+        printf "${yellow}%-20s${reset} : ${color}%s${reset}\n" "$key" "$value"
+    done
+
+    echo -e "--- FILES ---"
+    for key in "${!FILE[@]}"; do
+        value="${FILE[$key]}"
+        value="${value%"${value##*[![:space:]]}"}"  # Trim trailing whitespace
+        value="${value#"${value%%[![:space:]]*}"}"  # Trim leading whitespace
+        color="$reset"
+
+        if [[ $value == 'null' ]]; then
+            color=$purple    # Null value
+        elif [[ $value == '1' ]]; then
+            color=$green     # True value
+        elif [[ $value == '0' ]]; then
+            color=$red       # False value
+        fi
+
+        printf "${yellow}%-20s${reset} : ${color}%s${reset}\n" "$key" "$value"
+    done
 }
 
-uuid_or_filename() {
-    echo "uuid or filename? $1"
-}
+# helpers
 
-do_select() {
-    local uuids="${NAMESPACE[uuid]}"
-    local names="${NAMESPACE[name]}"
-    local select_all=$([[ -z "$uuids" && -z "$names" ]] && echo 'true' || echo 'false')
+get_env() { echo "${ENV[$1]}"; }
+get_file() { echo "${FILE[$1]}"; }
+get_error_msg() { echo "${ERROR[$1]}"; }
 
-    if [[ $select_all == "true" ]]; then
-        list_all_files
-    else
-        list_files_by_uuid $uuids
-        list_files_by_name $names
-    fi
-}
-
-list_all_files() {
-    echo "listing all"
-}
-
-list_files_by_uuid() {
-    echo "$@"
-}
-
-do_create() {
-    echo "new spreadsheet!"
-}
-
-do_edit() {
-    echo "opening in sc-im..."
-}
-
-do_delete() {
-    echo "removing file..."
-}
+# helpers
 
 main "$@"
